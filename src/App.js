8e5a1c4a614b3ffa33368308f0afbcb890940b37
indasashko123 <indasashko123@mail.ru>
2024-03-18 04:19:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,30 +8,31 @@ function App() {
 
 
   const [tg, setTg] = useState(window.Telegram.WebApp);
-  
-  const objString = JSON.stringify(tg);
-  console.log(objString);
+  const [objString, setobjString] = useState(JSON.stringify(tg));
+  const [ready, setReady] = useState(false);
+  const [initData, setinitData] = useState(tg.initData ? toString(tg.initData) : "nope");
+  const [initDataUnsafe, setinitDataUnsafe] = useState(tg.initDataUnsafe ? toString(tg.initDataUnsafe) : "nope");
+  const [themeParams, setthemeParams] = useState(tg.themeParams ? toString(tg.themeParams) : "nope");
 
-  //tg.showAlert(`Добро пожаловать, @${tg.WebAppUser.username}.`);
+
   useEffect( ()=> {
     tg.ready();
     setinitData(tg.initData ? toString(tg.initData) : "nope");
     setinitDataUnsafe(tg.initDataUnsafe ? toString(tg.initDataUnsafe) : "nope");
-    setthemeParams(tg.themeParams ? toString(tg.themeParams) : "nope");  
+    setthemeParams(tg.themeParams ? toString(tg.themeParams) : "nope");
+    setobjString(JSON.stringify(tg));
+    setReady(true);
   }, 
     []);
 
   const onClose = () => {
     tg.close();
   }
-  const [initData, setinitData] = useState(tg.initData ? toString(tg.initData) : "nope");
-  const [initDataUnsafe, setinitDataUnsafe] = useState(tg.initDataUnsafe ? toString(tg.initDataUnsafe) : "nope");
-  const [themeParams, setthemeParams] = useState(tg.themeParams ? toString(tg.themeParams) : "nope");
-
   return (
     <div className="App">
       <MainSreen props={tg}></MainSreen>
       <button onClick={onClose}>Close</button>
+      <p> { "Ready - " + ready}</p>
       <p> {objString}</p>
       <p> {initData}</p>
       <p> {initDataUnsafe}</p>
