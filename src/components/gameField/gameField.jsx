f65ba8ad7dccecf814aebb61321c81899572c936
indasashko123 <indasashko123@mail.ru>
2024-03-18 12:38:04 +0300

    Update gameField.jsx

--- src/components/gameField/gameField.jsx
+++ src/components/gameField/gameField.jsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 
 export const GameField = (props) => {
 
-  
+
   return  (
       <div className="GameFiled-Container">
         <div>
           <p> ID is {props.id}</p>
-          <p> Name id {props.first_name + " " + "\"props.username\" "+ props.last_name} </p>
+          <p> Name id {props.first_name + " " + props.username + " " + props.last_name} </p>
           <p> Language is {props.language_code}</p>
         </div>
         <img alt="no foto" src={props.photo}></img>
